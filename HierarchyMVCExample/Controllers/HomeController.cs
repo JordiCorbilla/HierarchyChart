568e1fedcc3cf5e0bf04feb10b9fb10cecb22eaa
JordiCorbilla <Jordi.coll.corbilla@gmail.com>
2016-11-09 16:09:30 +0000

    add ajax loader example on loading

--- HierarchyMVCExample/Controllers/HomeController.cs
+++ HierarchyMVCExample/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading;
 
 namespace HierarchyMVCExample.Controllers
 {
@@ -10,6 +11,7 @@ namespace HierarchyMVCExample.Controllers
     {
         public ActionResult Index()
         {
+            Thread.Sleep(5000);
             return View();
         }
 
